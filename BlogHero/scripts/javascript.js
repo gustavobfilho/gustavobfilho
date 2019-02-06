5181a389a6c4b3a1333b01dc34d7f60576bc0a10
Gustavo Bernardo da Silva Filho <gustavobfilho@gmail.com>
2019-02-06 15:04:21 -0300

    Adicionei BlogHero

---
+++ BlogHero/scripts/javascript.js
@@ -0,0 +1,80 @@
+var nomeAutor = "Gustavo Filho";
+
+var dataCriacaoBlog = formatDate(new Date());
+
+var sobreOblog = "O Blog foi criado para trazer informação sobre os Herois em quadrinhos pra aquelas pessoas que quando crianças tiveram um contato maior com esses desenhos e que eram acompanhados de perto por revistas e jibis, e que hoje pode ter uma dimensão bem maior dos super poderosos atraves das salas de Cinemas e Tv's no mundo todo. Com o advento das tecnologias e efeitos conseguidos atraves de computação grafica trazendo efeitos cada vez mais perfeitos ao nossos olhos. Tentamos aqui mostrar um pouquinho desse vasto universo, que traz os principais herois que marcaram sua epoca e que ainda continuam trazendo alegria e diversão aos fâs espalhados por todos os lugares. Espero que possam apreciar seu heroi predileto. Se você quer que seu Heroi preferido apareca nesse Blog, basta entrar em contato conosco no menu contato acima e informe o nome do seu heroi que tentaremos descobrir tudo a respeito desse heroi e colocaremos aqui para que voce possa apreciar. E se voce quiser se aprofundar ainda mais nesse universo, no menu acima tambem voce tem acesso aos sites do Universo Marvel e DC Comics.";
+
+var heroi1_nome = "Superman";
+
+var heroi1_data_postagem = formatDate(new Date());
+
+var heroi1_texto = "O Homem de Aço, o Último Filho de Krypton, o Homem do Amanhã, o Escoteiro Azul, o Homem que Tudo Tinha, o Super-Homem é, sem dúvida, o maior super-herói de todos os tempos. Não se trata de preferência, é simplesmente um fato. Você pode gostar mais do Batman, simpatizar mais com o Homem-Aranha ou se engajar com a Mulher-Maravilha, mas não pode negar que o Super-Homem representa a quintessência do heroísmo. O cineasta Quentin Tarantino, embora tenha sido sagaz, estava errado na tese que apresentou sobre o Super-Homem no segundo volume de “Kill Bill”. Kal-el, seu nome kriptoniano, não encarna Clark Kent para zombar dos humanos, mas para aplacar sua solidão de Deus Vivo. Ele é um ser inimaginavelmente poderoso, que poderia dominar o mundo se quisesse, mas que optou por ser um exemplo para a humanidade: mais humano, é melhor, do que os humanos. O conceito de Super-Homem permeia nosso imaginário coletivo: é Jesus, é Moisés, é Jeová, é Apolo, é Hercules e Sansão. Criado pelos artistas judeus Jerry Siegel e Joe Shuster, tendo sido publicado a primeira vez em 1938, o personagem sofreu diversas modificações ao longo das décadas. Já foi elétrico, foi dividido em dois, se casou, morreu e ressuscitou. Atualmente, perdeu uma de suas marcas registradas: a famosa cueca por cima das calças. Apesar de tudo isso seu S no peito permanece como um dos símbolos mais reconhecíveis do mundo. No cinema, Christopher Reeve é seu interprete definitivo. Nos quadrinhos, embora tenha muitos álbuns memoráveis, como “A Foice e o Martelo”, “Grandes Astros: Superman” e “As Quatro Estações”, sua melhor versão foi escrita e desenhada pela lenda dos quadrinhos John Byrne na segunda metade da década de 1980.";
+
+var heroi2_nome = "Homem-Aranha";
+
+var heroi2_data_postagem = formatDate(new Date());
+
+var heroi2_texto = "O Amigo da Vizinhança, o Cabeça de Teia. Cantem comigo: “Homem-Aranha, Homem-Aranha, nunca bate, só apanha”. Essa musiquinha, apesar de infame, resume muito da essência do personagem. O Homem-Aranha não é um super-herói solar e invencível como o Super-Homem, pelo contrário, precisa enfrentar muitas dificuldades, tanto na vida civil quanto na carreira de vigilante uniformizado. Criado pela dupla Stan Lee e Steve Ditko, publicado a primeira vez em 1962, a saga de Peter Parker se revelou ao longo das décadas um verdadeiro “bildungsroman”, um romance de formação. Praticamente todas as fases são importantes, tirando bobagens como a “Saga do Clone” e o “Duende Verde engravidando Gwen Stacy”. Um adolescente nerd adquire habilidades especiais e é tomado pelo orgulho, usando seus poderes egoisticamente e de forma irresponsável. Sofre uma tragédia, a morte de seu querido tio Ben, aprendendo da pior forma possível que grandes poderes geram grandes responsabilidades. Decide se tornar um protetor da cidade, dedicando sua vida a ajudar o próximo, mesmo à custa de muito sacrifício pessoal. A partir daí acompanhamos sua trajetória de altos e baixos, amores e desamores, perdas e ganhos. O vemos amadurecer não só como super-herói, mas principalmente como pessoa: de adolescente afoito passa a jovem adulto responsável e arrimo de família, torna-se professor e se casa, aos poucos ganha respeito como cientista e, finalmente, torna-se um rico e respeitável homem de negócios. Tudo isso sem abandonar seus velhos ideais. Tobey Maguire ainda é meu interprete preferido de Peter Parker, embora Tom Holland tenha bastante potencial. O playboy nutella encarnado por Andrew Garfield não era Peter Parker.";
+
+var heroi3_nome = "Batman";
+
+var heroi3_data_postagem = formatDate(new Date());
+
+var heroi3_texto = "“Seja sempre você mesmo, salvo se puder ser o Batman”. Os fãs do Morcego são uma verdadeira seita. Muitos vão reclamar do fato do Cavaleiro das Trevas, do Maior Detetive do Mundo, do Cruzado Embuçado, do Melhor Amigo dos Roteiristas não estar encabeçando a lista. Poderia estar, sobretudo pela popularidade do Super-Homem não estar em alta. Mas, pensando bem, não deixa de ser intrigante esse imenso carisma do Homem Morcego, considerando que ele é, em sua essência, um sonho molhado reacionário. Vejamos: um bilionário belo, recatado e do lar resolve vestir uma máscara para fazer justiça com as próprias mãos, espancando criminosos pobres e doentes mentais fantasiados que lideram o submundo. Vale tudo, de causar traumas psicológicos profundos até fraturas expostas, só não vale matar. Não é isso? Mas vamos parar com sociologia barata. Desce o cacete na bandidagem Bruce! Criado por Bob Kane em 1939, talvez Batman seja o personagem de quadrinhos com o maior número de obras memoráveis, como os álbuns “A Piada Mortal”, “O Messias”, “O Longo Dia das Bruxas”, “Terra Um”, “O Filho do Demônio” e “Morte em Família”, mas seu interprete quintessencial permanece sendo o reaça genial Frank Miller, autor de “Batman: Ano Um” e “O Retorno do Cavaleiro das Trevas”. Esse Batman invencível, calculista, durão e um tanto sádico que conhecemos hoje foi estabelecido por ele. No cinema Batman tornou-se uma espécie de James Bond, com diversos atores se revezando no papel. Podemos resumir assim: o bom ator Michael Keaton foi mal escalado, Val Kilmer não conta (Bruce Wayne loiro?), George Clooney foi um desperdício, Christian Bale é quase tão maluco quanto Bruce Wayne, Ben Affleck é o mais bombado e, claro, o hilário Adam West é o melhor dançarino.";
+
+var heroi4_nome = "Mulher-Maravilha";
+
+var heroi4_data_postagem = formatDate(new Date());
+
+var heroi4_texto = "A Princesa das Amazonas foi criada em 1941 pelo psicólogo e inventor William Moulton Marston como um símbolo da força feminina. Uma leitura contemporânea de seus primeiros quadrinhos vai revelar diversas incongruências e certo machismo involuntário. Talvez por isso a Mulher-Maravilha tenha atravessado décadas como uma personagem com importância mais simbólica e política do que estética até o ano de 1987, quando o escritor e desenhista George Perez apresentou sua extraordinária interpretação da saga da Mulher-Maravilha, aproximando-a de modo definitivo dos mitos gregos. E sua versão definitiva, sem esquecer as ótimas contribuições de roteiristas posteriores como Gail Simone, Straczynski e Grant Morrison. No audiovisual, para meu eterno lamento, a italiana Mônica Bellucci é a “Mulher-Maravilha perfeita que não foi”. Por isso a linda Lynda Carter segue no imaginário coletivo como a dona da personagem, apesar da série que protagonizava ser de gosto duvidoso. Gal Gadot, que encarna a princesa Diana no cinema, deve assumir o manto de Carter para as próximas gerações.";
+
+var heroi5_nome = "Homem-de-Ferro";
+
+var heroi5_data_postagem = formatDate(new Date());
+
+var heroi5_texto = "“Playboy, bilionário, gênio, filantropo.” Caso raro na qual a versão cinematográfica redefiniu o personagem dos quadrinhos. Robert Downey Jr. é Tony Stark, Tony Stark é Robert Downey Jr. Quando foi criado por Stan Lee e Jack Kirby em 1963, no contexto da Guerra Fria, o personagem era bastante bidimensional, permanecendo assim por décadas, com poucas histórias relevantes, como “O Demônio na Garrafa”, que aborda seu alcoolismo. A partir de 2002, com a reinvenção dos Vingadores na série “Os Supremos”, escrita por Mark Miller e desenhada por Bryan Hitch, o Homem de Ferro ganhou novo fôlego nos quadrinhos, talvez até inspirando a interpretação de Downey Jr. A partir daí Tony Stark tornou-se o bom canalha, o escroque egoísta que está no lado certo. O que provou na série “Guerra Civil”. Para todos os efeitos, o Homem de Ferro era o vilão, enfrentando um idealista, mas equivocado, Capitão América. Afinal, um indivíduo com superpoderes é sim uma arma que deve ser registrada e monitorada em nome da segurança pública. O mais interessante do personagem é que a armadura é apenas uma parte do que o torna um super-herói, não seu elemento definidor. Mesmo sem a armadura, Tony Stark continua sendo um “playboy, bilionário, gênio, filantropo”. Como superar isso?";
+
+var heroi6_nome = "Capitão America";
+
+var heroi6_data_postagem = formatDate(new Date());
+
+var heroi6_texto = "A popularidade do Bandeiroso está em alta. Para cada pessoa que diz que detesta o Sentinela da Liberdade outras duas desfilam com camisetas com a logo do escudo. Geralmente, quem antipatiza com o Capitão América justifica sua atitude pelo fato do personagem ser WASP, imperialista, capitalista, patriota, além de muito “certinho” e “arrumadinho”. Esses “motivos” fazem tanto sentido quanto criticar o papa por ser católico. Em todo caso, em nossos tempos líquidos pós-modernos com sabor de suco de groselha um personagem que está, literalmente, fora de seu tempo, não pode ser mesmo unanimidade. Criado em 1940 pela dupla Joe Simon e Jack Kirby no contexto da Segunda Grande Guerra, o Capitão América é sempre representado como uma figura de valores anacrônicos, estando na América hippie dos anos 1960, na psicodélica década de 1970, na era yuppie e cyberpunk de 1980, na década perdida de 1990 ou na era digital do século 21. Curiosamente, sua origem está ligada a discussões sobre como a ciência pode modificar e melhorar o ser humano, algo muito moderno. Mas subliminarmente sempre será uma arma de guerra, vide sua versão atualizada e barra-pesada apresentada na série “Os Supremos” (2002), escrita por Mark Miller e desenhada por Bryan Hitch, na qual é mostrado como um verdadeiro militar, não um soldadinho de chumbo. No arco “Guerra Civil”, onde “morre” no final, temos a real dimensão dos motivos porque ele é uma inspiração para toda a comunidade de super-heróis da Marvel. No cinema, foi interpretado de maneira bisonha pelo filho canastrão do lendário escritor J. D. Salinger, além de outras encarnações igualmente esquecíveis. Considerando isso, os esforços de Chris Evans o colocam como o melhor Capitão América até o momento. Nada brilhante, mas não passa vergonha.";
+
+var heroi7_nome = "Incrivel Hulk";
+
+var heroi7_data_postagem = formatDate(new Date());
+
+var heroi7_texto = "O Gigante Esmeralda é um dos super-heróis preferidos das crianças. E também dos adultos meio velhacos que se lembram da antiga série de televisão com o halterofilista Lou Ferrigno pintado toscamente de verde. O pianinho triste que tocava ao final de cada episódio é emblemático. De fato, desde quando foi criado por Stan Lee e Jack Kirby em 1962, o Hulk é um personagem trágico, uma espécie de “o médico e o monstro” moderno. Essa característica rendeu diversas histórias em quadrinhos cheias de ação, mas também altamente dramáticas e com considerável carga psicológica. “Planeta Hulk”, “Hulk Contra o Mundo”, “Futuro Imperfeito” e “Hulk Cinza” são bons exemplos, assim como a curta fase escrita e desenhado por John Byrne e seu período banido na “Encruzilhada”. Se é consenso que a computação gráfica superou a interpretação bidimensional de Lou Ferrigno, qual ator interpretou melhor o alter ego do Golias Verde? Quem foi o melhor doutor David Bruce Banner? Apesar dos dignos esforços de Edward Norton e Mark Ruffalo, fico com a fragilidade de Bill Bixby e suas lentes de contato verdes saltados. Toca a pianinho triste de novo, Sam.";
+
+var heroi8_nome = "Thor";
+
+var heroi8_data_postagem = formatDate(new Date());
+
+var heroi8_texto = "O bom e velho Stan Lee foi mesmo genial quando desenvolveu a ideia de transformar um deus do obscuro panteão nórdico em super-herói lá pelos idos de 1962. Qualquer pessoa menos criativa reciclaria Hércules, Sansão ou outro fortão genérico. No cinema, o ator Chris Hemsworth, o pescador parrudo de Asgard, embora nenhum dos filmes seja memorável, nasceu para o papel. Certamente, não é o brutamontes ruivo da mitologia, mas é perfeito para encarnar o galante príncipe louro da Marvel. Algumas das melhores histórias do Filho de Odin foram escritas pelo próprio Stan Lee, mas sua melhor fase foi a produzida por Walt Simonson na década de 1980, com cada edição sendo mais surpreendente que a outra, chegando ao limite do Deus do Trovão se transformar no Sapo Thor. Magnificamente psicodélico! Pais, proteja seus filhos das drogas; dê quadrinhos para eles lerem.";
+
+var heroi9_nome = "Lanterna Verde";
+
+var heroi9_data_postagem = formatDate(new Date());
+
+var heroi9_texto = "Hal Jordan continuaria a ajudar os Lanternas Verdes a manter a ordem no Universo, se responsabilizando pelo setor 2814. Combateu várias ameaças, ajudou a fundar a Liga da Justiça, e conquistou o título de maior Lanterna Verde de todos os tempos. Vários foram os inimigos que conquistou ao longo do caminho, sendo o mais insistente deles o já citado Sinestro. O vilão não ficou se lamentando após seu exílio. Fez alianças com os habitantes do Universo de Antimatéria e forjou para si um novo anel de poder amarelo, a cor que é a fraqueza dos anéis verdes. A energia do anel também pode ser moldada nas mais diferentes formas, de acordo com a criatividade do Lanterna Verde que estiver usando. Pode assumir a forma de seres vivos, armas, ferramentas, veículos, e o que mais decidirem.";
+
+var heroi10_nome = "Flash";
+
+var heroi10_data_postagem = formatDate(new Date());
+
+var heroi10_texto = "Flash, provavelmente, é o mais poderoso de todos os super-heróis. E também um dos mais difíceis de ser roteirizado. O sujeito é mais rápido do que a luz! É capaz de viajar no tempo e entre as dimensões! Como produzir uma aventura dramaticamente razoável que dure mais do que duas páginas? Talvez por isso Flash tenha poucas histórias clássicas, sendo as mais importantes sua morte durante a “Crise nas Infinitas Terras” e “Flashpoint”, justamente duas sagas que se propõem reconstruir o universo DC. Trata-se de um personagem extremo tratado de modo ingênuo. Flash é, sobretudo, um conceito fascinante e uma logo legal. O Flash original, àquele com um capacete parecido com uma panela, foi criado por Gardner Fox e Harry Lampert em 1940, com o nome de Jay Garrick, mas sua versão clássica é mesmo Barry Allen, criado pelo mesmo Gardner Fox em 1956. Depois dele, assumiram o manto mais dois velocistas: Wally West e Bart Allen. No cinema ou na TV nenhum ator conseguiu ser o Flash definitivo, talvez porque, na prática, ninguém se importa.";
+
+function formatDate(date) {
+  var monthNames = [
+    "Janeiro", "Fevereiro", "Março",
+    "Abril", "Maio", "Junho", "Julho",
+    "Agosto", "Setembro", "Outubro",
+    "Novembro", "Dezembro"
+  ];
+
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
+
+  return day + ' ' + monthNames[monthIndex] + ' ' + year;
+}
